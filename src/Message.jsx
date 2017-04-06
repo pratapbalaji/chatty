abda5766722e333574f233af12bb16b983629a55
Vinay Balaji <vinaybalaji@gmail.com>
2017-04-06 21:29:12 +0000

    Added display-image feature

--- src/Message.jsx
+++ src/Message.jsx
@@ -3,16 +3,34 @@ import React, {Component} from 'react';
 class Message extends Component {
   constructor() {
     super();
+    this.messageContainsImage = this.messageContainsImage.bind(this);
+  }
+
+  messageContainsImage(message) {
+    if (message.includes(".jpg", ".jpeg", ".png")) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   render() {
-    return (
+    if(this.messageContainsImage(this.props.message.message) === true)
+    {
+      return (
       <div className={this.props.message.cssClass}>
-        //{console.log(this.props.message.usercolor)}
+        <span className="message-username" style={{color: this.props.message.usercolor}}>{this.props.message.user}</span>
+        <span><img className="message-image" src = {this.props.message.message} /></span>
+      </div>
+      );
+    } else {
+      return (
+      <div className={this.props.message.cssClass}>
         <span className="message-username" style={{color: this.props.message.usercolor}}>{this.props.message.user}</span>
         <span className="message-content">{this.props.message.message}</span>
       </div>
-    );
+      );
+    }
   }
 }
 export default Message;
